fc00b005e6eb9aaa375400aed5943324b73d6a8f
Howard Chung <howardzchung@gmail.com>
2024-09-01 08:43:12 +0000

    return json

--- functions/stats.js
+++ functions/stats.js
@@ -1,5 +1,5 @@
 export default async (req, context) => {
   const resp = await fetch("http://azure.howardchung.net:8081/api/stats");
   const data = await resp.json();
-  return new Response(data);
+  return new Response.json(data);
 };
