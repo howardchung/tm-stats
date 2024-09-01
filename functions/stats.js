091d468c4d7f786253c05648bccdaebd2dd4c1d9
Howard Chung <howardzchung@gmail.com>
2024-09-01 08:49:18 +0000

    cors

--- functions/stats.js
+++ functions/stats.js
@@ -1,5 +1,7 @@
 export default async (req, context) => {
   const resp = await fetch("http://azure.howardchung.net:8081/api/stats");
   const data = await resp.json();
-  return Response.json(data);
+  return Response.json(data, {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  });
 };
