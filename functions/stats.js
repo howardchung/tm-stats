3365f6b2eec942001a4b53a142f7f28b7143dbdc
Howard Chung <howardzchung@gmail.com>
2025-01-04 03:28:03 -0800

    Update stats.js

--- functions/stats.js
+++ functions/stats.js
@@ -1,5 +1,5 @@
 export default async (req, context) => {
-  const resp = await fetch("http://azure.howardchung.net:8081/api/stats");
+  const resp = await fetch("http://azure.howardchung.net:8081/api/statspage");
   const data = await resp.json();
   return Response.json(data, {
     headers: { "Access-Control-Allow-Origin": "*" },
